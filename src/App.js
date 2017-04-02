acc9dff8dc12216ddad8c6356a7fb590f319c542
Sheon Han <sheon.han@gmail.com>
2017-04-02 19:38:01 -0400

    Added columns

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Col } from 'react-bootstrap';
 import './App.css';
 import Chart from './Chart'
 import Pi from './Pi'
+import Column from './Column'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Col md={6} mdPull={6} />
-        <Col md={6} mdPush={6} />
       <Chart />
-      <Pi />
+      <Pi /> 
+      <Column />
       </div>  
     );
   }
---
+++ src/Column.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Column extends Component {
+  render() {
+    return (
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1">
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+  }
+}
+
+export default Column;
