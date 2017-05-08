3d464690ed8b864936395e71c772be1be9e704f6
Ji-Sung Kim <jisungk@users.noreply.github.com>
2017-05-07 23:18:23 -0400

    Beautify login, logout, plots
    # NEED TO DO: 
    - limit bar plot height (way too tall, overflows)
    - fix dropdown control
    - make functional logout button
    - make simple user auth
    - center pie chart, time series
    - add labels and axis titles to pie chart, time series
    - fix colors of plots
    - add export to csv link with new lambda function
    - add team bio pics

--- client/src/Perceival.js
+++ client/src/Perceival.js
@@ -5,13 +5,11 @@ import React, { Component } from 'react';
 import Barchart from './components/Barchart';
 import Piechart from './components/Piechart';
 import Linechart from './components/Linechart';
-import Nav from './components/Nav';
 
 class Perceival extends Component {
     render() {
         return (
             <div>
-                <Nav />
                 <div id='row1' className='row'>
                     <div className='col-md-6'>
                         <div className='card'>
@@ -33,12 +31,6 @@ class Perceival extends Component {
                         <div className='card'>
                             <div className='content'>
                                 <Barchart />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6">
-                        <div className="card">
-                            <div className="content">
                             </div>
                         </div>
                     </div>
--- client/src/components/Main/Login/Login.js
+++ client/src/components/Main/Login/Login.js
@@ -1,18 +1,40 @@
 import React, { PropTypes as T } from 'react'
 import {ButtonToolbar, Button} from 'react-bootstrap'
 import AuthService from '../../../utils/AuthService'
-import styles from './styles.module.css'
+import './styles.module.css'
+
+import Typist from 'react-typist'
+import Particles from 'react-particles-js';
 
 export class Login extends React.Component {
-
   render() {
     const { auth } = this.props
+
     return (
-      <div className={styles.root}>
-        <h2>Login</h2>
-        <ButtonToolbar className={styles.toolbar}>
-          <Button bsStyle="primary" onClick={auth.login.bind(this)}>Login</Button>
-        </ButtonToolbar>
+      <div className="root">
+          <div className="col-md-12 login">
+            <div className="above-particles"> 
+              <h1>
+                <Typist avgTypingSpeed={90} cursor={{hideWhenDone: true, hideWhenDoneDelay: 120}}>
+                  Hi! I'm <span className="purple text-semibold">Percy</span>
+                </Typist>
+              </h1>
+              <h4>Please login to your acccount</h4>
+              <ButtonToolbar className="login-toolbar">
+                <Button bsStyle="primary" onClick={auth.login.bind(this)}>Authenticate</Button>
+              </ButtonToolbar>
+            </div>
+
+            <div>
+              <div className="particles">
+                <Particles params={{particles: {
+                    number: {value: 100}, color: {value: "#9013FE"}, size: {value: 3}, 
+                    opacity: {value: 0.2}, line_linked: {color: "#9013FE", width: 1.2, opacity: 0.2}
+                  }}}
+                />
+              </div>
+            </div>
+          </div>
       </div>
     )
   }
