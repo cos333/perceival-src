a82681bcb242ab8e83b9b86056e6a0464246b4ea
Sheon Han <sheon.han@gmail.com>
2017-05-14 16:03:11 -0400

    Add register button

--- client/src/Perceival.js
+++ client/src/Perceival.js
@@ -1,46 +1,52 @@
 import './Perceival.css';
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import Barchart from './components/Barchart';
 import Export from './components/Export';
 import Linechart from './components/Linechart';
 import Piechart from './components/Piechart';
+import Register from './components/Register';
 
 class Perceival extends Component {
-    
-  render() {
+
+    render() {
         return (
             <div>
-                <div id='row1' className='row'> 
-                    <div className='col-md-6'> 
-                        <div className='card'> 
-                            <div className='content'> 
-                                <Piechart/> 
-                            </div> 
-                        </div> 
-                    </div> 
-                    <div className="col-md-6"> 
-                        <div className="card"> 
-                            <div className="content"> 
-                                <Barchart /> 
-                            </div> 
-                        </div> 
-                    </div> 
-                </div> 
-                <div id='row2' className='row'> 
-                    <div className='col-md-12'> 
-                        <div className='card'> 
-                            <div className='content'> 
-                                <Linechart /> 
-                            </div> 
-                        </div> 
-                    </div> 
-                </div>
-                <div id='row2' className='row'> 
+                <div id='row0' className='row'>
                     <div className='col-md-12'>
-                        <Export auth={this.props.auth}/> 
-                    </div> 
+                            <Register />
+                    </div>
+                </div>
+                <div id='row1' className='row'>
+                    <div className='col-md-6'>
+                        <div className='card'>
+                            <div className='content'>
+                                <Piechart />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="content">
+                                <Barchart />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id='row2' className='row'>
+                    <div className='col-md-12'>
+                        <div className='card'>
+                            <div className='content'>
+                                <Linechart />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id='row2' className='row'>
+                    <div className='col-md-12'>
+                        <Export auth={this.props.auth} />
+                    </div>
                 </div>
             </div>
         );
