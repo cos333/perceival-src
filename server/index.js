c92e5ee5dc31d4eb785dd50208767e388b9ed8a4
Sheon Han <sheon.han@gmail.com>
2017-05-14 09:47:12 -0400

    Add bodyparser

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../client/build')));
