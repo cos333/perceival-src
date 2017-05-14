a9fd83883c013d0f1efc5d89a0394515ca25c22c
Sheon Han <sheon.han@gmail.com>
2017-05-14 08:57:38 -0400

    Add test.py

---
+++ test.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+'''
+TODO: add authorization via the header
+'''
+
+# url format = https://[my-api-id].execute-api.[region-id].amazonaws.com/[stage]/[resource]
+url = 'https://dil2yon0pd.execute-api.us-west-2.amazonaws.com/prod/getPlotData'
+
+# r = requests.post(url, data={'plot': 'pie', 'response': 'centsspent', 'segment': 'language'})
+r = requests.post(url, data={'plot': 'timeseries', 'response': 'centsspent', 'segment': 'age', 'display': 'sum'})
+
+print('Status code: ' + str(r.status_code))
+print(r.text[1:-1]) # strips quotation marks @ first and last index by subsetting
+print ''
+print(r.headers) # strips quotation marks @ first and last index by subsetting
+
+'''
+{
+  "body": {
+      "plot": "bar",
+      "response": "SecondsSpent",
+      "segment": "age"
+  }
+}
+'''
