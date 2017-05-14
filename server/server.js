0ebfc9d801a8bf5cb549b31d7aa555cb64cfd917
Sheon Han <sheon.han@gmail.com>
2017-05-14 17:34:10 -0400

    Clean up directories

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser')
 
-    const app = express();
-const PORT = process.env.PORT || 5000;
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
