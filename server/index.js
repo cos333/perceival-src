e3f5b79f367822f88888571abfe667c0ef976985
Sheon Han <sheon.han@gmail.com>
2017-05-14 20:25:21 -0400

    Resolve routes

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser')
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Answer API requests.
-app.get('/api', function(req, res) {
+app.get('/api', function (req, res) {
   res.set('Content-Type', 'application/json');
   res.send('{"message":"Hello from the custom server!"}');
 });
@@ -22,6 +22,6 @@ app.get('*', function(request, response) {
   response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}`);
 });
